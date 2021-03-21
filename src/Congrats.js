190aeb5c0986b420c31c20952653da377bd87196
ESP12 <helias@DESKTOP-POGPB3M.localdomain>
2021-03-21 20:50:49 +0800

    Add Congrats component Add Congrats component test Add testUtils

--- src/Congrats.js
+++ src/Congrats.js
@@ -1,13 +1,24 @@
 import React from 'react';
 
 /**
- * Functional react component 
+ * Functional react component component form congratulatory message
+ * @function
+ * 
  */
 
-export default () => {
-  return (
-    <>
-    
-    </>
-  ) 
+export default (props) => {
+
+  if(props.success) {
+    return (
+      <div data-test="component-congrats">
+        <span data-test="congrats-message">
+          Congratulations! you guessed the word!
+        </span>
+      </div>
+    );
+  } else {
+    return (
+      <div data-test="component-congrats"/>
+    )
+  } 
 }
